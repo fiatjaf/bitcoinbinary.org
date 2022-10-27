7214a85e5c3fa04e74605f1cbedeab54dc0cfc06
fiatjaf <fiatjaf@gmail.com>
2022-10-27 07:48:05 -0300

    artifacts as merely sourceable files.

--- blockstream-green/artifacts.sh
+++ blockstream-green/artifacts.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 DATE=`date +%Y-%m-%d`
 TWITTER_NAME="@Blockstream Green"
 URL="https://blockstream.com/"
@@ -13,7 +11,3 @@ SHA256=`shasum -a 256 green_android/green/build/outputs/apk/development/release/
 # Note GITHUB_ environment variables are populated by Github Actions
 ARTIFACT_BASEURL="https://github.com/${GITHUB_REPOSITORY}/raw"
 ARTIFACT_BRANCH=${GITHUB_REF_NAME}
-
-ENTRY_TO_APPEND="<li><a href='${REPO}/releases/tag/${VERSION_STRING}'>${DATE}</a> | <a href='${URL}' class='project-name'>${PROJECT}</a>  | <a href='${REPO}/releases/tag/${VERSION_STRING}'>v${VERSION}</a> | <a href='${CHECKSUM_SOURCE}'> factory ${SHA256} </a>| <a href='${ARTIFACT_BASEURL}/${ARTIFACT_BRANCH}/${PROJECT}/${PROJECT}-${VERSION}-video.webm'>video proof</a> | <a href='https://github.com/coinkite/bitcoinbinary.org/blob/main/${PROJECT}/artifacts.sh' class="bot">build bot</a></li>"
-
-echo ${ENTRY_TO_APPEND}
--- simple-bitcoin-wallet/artifacts.sh
+++ simple-bitcoin-wallet/artifacts.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 DATE=`date +%Y-%m-%d`
 TWITTER_NAME="@SimpleBtcWallet"
 URL="https://sbw.app/"
@@ -12,7 +10,3 @@ SHA256=`shasum -a 256 wallet/app/build/outputs/apk/release/SBW-${VERSION}.apk | 
 # Note GITHUB_ environment variables are populated by Github Actions
 ARTIFACT_BASEURL="https://github.com/${GITHUB_REPOSITORY}/raw"
 ARTIFACT_BRANCH=${GITHUB_REF_NAME}
-
-ENTRY_TO_APPEND="<li><a href='${REPO}/releases/tag/${VERSION}'>${DATE}</a> | <a href='${URL}' class='project-name'>${PROJECT}</a>  | <a href='${REPO}/releases/tag/${VERSION}'>${VERSION}</a> | <a href='${CHECKSUM_SOURCE}'> factory ${SHA256} </a>| <a href='${ARTIFACT_BASEURL}/${ARTIFACT_BRANCH}/${PROJECT}/${PROJECT}-${VERSION}-video.webm'>video proof</a> | <a href='https://github.com/coinkite/bitcoinbinary.org/blob/main/${PROJECT}/artifacts.sh' class="bot">build bot</a></li>"
-
-echo ${ENTRY_TO_APPEND}
